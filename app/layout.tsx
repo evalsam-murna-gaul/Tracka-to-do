ad2c773e841c7d85b3757d74a5a59335d0c94cd1
Evalsam Murna Gaul <evalsamgaul@gmail.com>
2026-02-06 18:28:33 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'My To-do App',
+  title: 'Tracka App',
   description: 'Simple and efficient, keeps you productive',
 };
 
